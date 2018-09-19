769cc1d04a2e9835694b27b47715bcb539cf64d7
FJ7I4YEUOQ7Q21K\Administrator <742117806@qq.com>
2018-09-19 10:42:22 +0800

    添加项目工程程序和相关资料

---
+++ AI_扩展板MCU固件程序/STM32F103C8T6/Drivers/BSP/Inc/spi.h
@@ -0,0 +1,20 @@
+
+#ifndef __SPI_H__
+#define __SPI_H__
+#include "stm32f1xx.h"
+
+#define WIRELESS_NSEL_L()		(HAL_GPIO_WritePin(SI4438_NSS_GPIO_Port, SI4438_NSS_Pin, GPIO_PIN_RESET))
+#define WIRELESS_NSEL_H()		(HAL_GPIO_WritePin(SI4438_NSS_GPIO_Port, SI4438_NSS_Pin, GPIO_PIN_SET))
+#define SDN_L()					(HAL_GPIO_WritePin(SI4438_SDN_GPIO_Port, SI4438_SDN_Pin, GPIO_PIN_RESET))
+#define SDN_H()					(HAL_GPIO_WritePin(SI4438_SDN_GPIO_Port, SI4438_SDN_Pin, GPIO_PIN_SET))
+
+uint8_t SPI_RWbyte(uint8_t sdata);
+
+
+void WIRELESS_SPI_Init(void);
+uint8_t SPI_RWbyte(uint8_t sdata);
+
+
+
+#endif
+
